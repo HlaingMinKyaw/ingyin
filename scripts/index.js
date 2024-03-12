f469cc524d49ab359958034dc70ab7fa5bdf8de1
HlaingMinKyaw <jellytonelay11116@gmail.com>
2024-03-12 20:43:25 +0630

    ff

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Mar 15, 2024',
+  birthdate: 'Mar 15, 2023',
   name: '❤	Khin Sandi Htet ❤	'
 };
 
